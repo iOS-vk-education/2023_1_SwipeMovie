4a87e606839cbabc33a4643d11a7b2fc11dd6017
Apollonoff <apollonoff.arseniy@gmail.com>
2023-10-26 15:55:32 +0300

    Create a project

---
+++ SwipeMovie/SwipeMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeMovie
+//
+//  Created by Arseniy Apollonov on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
