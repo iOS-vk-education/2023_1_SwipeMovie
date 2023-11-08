796a32ff0b62a1cf8e9a08dfd21ad11857f02a67
Egor Ivanov <101597786+Ozondo@users.noreply.github.com>
2023-11-08 16:05:37 +0300

    v3

--- SwipeMovie/SwipeMovie/Modules/TabBar/TabBarController.swift
+++ SwipeMovie/SwipeMovie/Modules/TabBar/TabBarController.swift
@@ -1,23 +1,37 @@
 import UIKit
 
-func createTabBarController(UIViewController: UIViewController, title: String?, image: UIImage?, tag: Int) -> UINavigationController {
-    let tabBarElement = UINavigationController(rootViewController: UIViewController)
-    tabBarElement.tabBarItem = UITabBarItem(title: title, image: image, tag: tag)
+func createTabBarController(controller: UIViewController, title: String?, image: String, tag: Int) -> UINavigationController {
+    let tabBarElement = UINavigationController(rootViewController: controller)
+    tabBarElement.tabBarItem = UITabBarItem(title: title, image: UIImage(systemName: image), tag: tag)
     return tabBarElement
 }
 
-enum TabBarController {
+final class TabBarController {
     static func createTabBar() -> UITabBarController {
         UITabBar.appearance().barTintColor = .white
         UITabBar.appearance().tintColor = .white
         UITabBar.appearance().backgroundColor = UIColor(named: "tabBarViewColor")
         let tabBarController = UITabBarController()
-        let filmListTabBarController = createTabBarController(UIViewController: FilmListViewController(), title: "Списки фильмов", image: UIImage(systemName: "film.fill"), tag: 0)
-        let mainMenuTabBarController = createTabBarController(UIViewController: MainMenuViewController(), title: "Главное меню", image: UIImage(systemName: "checkmark.rectangle.stack.fill"), tag: 1)
-        let historyTabBarController = createTabBarController(UIViewController: HistoryViewController(), title: "История", image: UIImage(systemName: "clock.fill"), tag: 2)
-        tabBarController.setViewControllers([filmListTabBarController,
-                                             mainMenuTabBarController,
-                                             historyTabBarController], animated: true)
+        let filmListTabBarController = createTabBarController(
+            controller: FilmListViewController(),
+            title: "Списки фильмов",
+            image: "film.fill",
+            tag: 0)
+        let mainMenuTabBarController = createTabBarController(
+            controller: MainMenuViewController(),
+            title: "Главное меню",
+            image: "checkmark.rectangle.stack.fill",
+            tag: 1)
+        let historyTabBarController = createTabBarController(
+            controller: HistoryViewController(),
+            title: "История",
+            image: "clock.fill",
+            tag: 2)
+        tabBarController.setViewControllers([
+            filmListTabBarController,
+            mainMenuTabBarController,
+            historyTabBarController],
+            animated: true)
         return tabBarController
     }
 }
