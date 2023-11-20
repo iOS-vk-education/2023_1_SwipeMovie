57851c39cb6c146f22a63af88d857bba92040c94
Apollonoff <apollonoff.arseniy@gmail.com>
2023-11-20 20:52:16 +0300

    Create HistoryView

--- SwipeMovie/SwipeMovie/Modules/FilmList/FilmListViewController.swift
+++ SwipeMovie/SwipeMovie/Modules/FilmList/FilmListViewController.swift
@@ -8,14 +8,6 @@
 import UIKit
 
 class FilmListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
     /*
     // MARK: - Navigation
 
--- SwipeMovie/SwipeMovie/Modules/History/HistoryViewController.swift
+++ SwipeMovie/SwipeMovie/Modules/History/HistoryViewController.swift
@@ -8,22 +8,118 @@
 import UIKit
 
 class HistoryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    let historyTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    private func configureLeftBarLabel() {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        label.text = "История"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: label)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureRightBarButton() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Изменить",
+                                                                 style: .plain,
+                                                                 target: self,
+                                                                 action: #selector(changeButtonDidTapped))
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor(named: "swipeMovieWhite")
     }
-    */
+    
+    @objc
+    func changeButtonDidTapped() {
+        //
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureLeftBarLabel()
+        configureRightBarButton()
+        
+        self.view.addSubview(historyTableView)
+        
+        historyTableView.backgroundColor = UIColor(named: "swipeMovieBlue")
+        
+        // fix white space in top of tableview while scrolling
+        historyTableView.bounces = false
+        
+        // fix empty space in the table view bottom
+        let footerView = UIView()
+        footerView.frame.size.height = 0
+        historyTableView.tableFooterView = footerView
+        
+        historyTableView.tableHeaderView = searchController.searchBar
+        searchController.searchBar.backgroundColor = UIColor(named: "swipeMovieBlue")
+        
+        // constrain the table view
+        NSLayoutConstraint.activate([
+            historyTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            historyTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            historyTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            historyTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        
+        // set tableview delegate and data source
+        historyTableView.delegate = self
+        historyTableView.dataSource = self
+        
+        // register custom cell
+        historyTableView.register(MovieCustomCell.self, forCellReuseIdentifier: "MovieCell")
+        
+        // set search bar delegate
+        searchController.searchBar.delegate = self
+        
+        // configure search bar
+        searchController.searchBar.placeholder = "Поиск"
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.isTranslucent = true
+        searchController.searchBar.barTintColor = UIColor(named: "swipeMovieBlue")
+        searchController.searchBar.searchTextField.backgroundColor = .white
+        searchController.searchBar.tintColor = .white
+        
+    }
+    
+}
 
+// MARK: - TableViewDelegate and TableViewDataSource
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // swiftlint:disable force_cast
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as!  MovieCustomCell
+        // swiftlint:enable force_cast
+        
+        cell.selectionStyle = .none
+        // test
+        cell.lobbyNameLabel.text = "Lobby Name"
+        cell.movieNameLabel.text = "Movie Name"
+        cell.movieDescriptionLabel.text = "Movie Description"
+        cell.movieImage.image = UIImage(named: "AppIcon")
+        //
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 118
+    }
+}
+
+// MARK: - SearchBarDelegate
+extension HistoryViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //
+    }
 }
